8bc266937d1fc5eca7acbe69b7945b4cb0ec7ff3
Renato Spakauskas <renatospaka@gmail.com>
2023-04-05 16:36:59 -0300

    feat: :necktie: product repository
    create, findAll, findByID, update, delete methods

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,27 +7,27 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.00, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("Product 1", 0)
+	p, err := NewProduct("Product 1", 0.00)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
 func TestProductWhenNameIsInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.00)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
