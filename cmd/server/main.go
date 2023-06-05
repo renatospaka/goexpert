e567d32d47339ea5a90d9b00ca1c5dc0ac52ba64
Renato Spakauskas <renatospaka@gmail.com>
2023-06-05 10:46:44 -0300

    feat: :sparkles: adding middleware to scure routes
    securing products routes through JWT token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/renatospaka/library/configs"
 	"github.com/renatospaka/library/internal/entity"
 	"github.com/renatospaka/library/internal/infra/database"
@@ -29,19 +30,24 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
-	productDB := database.NewProduct(db)
-	ProductHandler := handlers.NewProductHandler(productDB)
-	r.Post("/products", ProductHandler.CreateProduct)
-	r.Get("/products", ProductHandler.GetProducts)
-	r.Get("/products/{id}", ProductHandler.GetProduct)
-	r.Put("/products/{id}", ProductHandler.UpdateProduct)
-	r.Delete("/products/{id}", ProductHandler.DeleteProduct)
 	
 	userDB := database.NewUser(db)
 	UserHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JWTExpiresIn)
 	r.Post("/users", UserHandler.Create)
 	r.Post("/users/generate_token", UserHandler.GetJWT)
+
+	productDB := database.NewProduct(db)
+	ProductHandler := handlers.NewProductHandler(productDB)
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/", ProductHandler.CreateProduct)
+		r.Get("/", ProductHandler.GetProducts)
+		r.Get("/{id}", ProductHandler.GetProduct)
+		r.Put("/{id}", ProductHandler.UpdateProduct)
+		r.Delete("/{id}", ProductHandler.DeleteProduct)		
+	})
 
 	log.Println("servidor escutando porta:", 8000)
 	http.ListenAndServe(":8000", r)
